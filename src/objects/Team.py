7f1b7f8b2978b52efb7941f42f74b6df04fff3e1
Daniel Harris <dan.harris686@gmail.com>
2025-06-16 12:49:28 +0100

    Add team recent form function

--- src/objects/Team.py
+++ src/objects/Team.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
+
 class Team:
-    def __init__(self, name:str, df:pd.DataFrame):
+    def __init__(self, name: str, df: pd.DataFrame):
         self.name = name
         self.df = df
 
@@ -11,61 +12,97 @@ class Team:
 
         Parameters
         ----------
-        num_matches: int, optional 
-            Number of matches used to calculate recent form. Defaults to 5. 
-        
-        Returns 
+        num_matches: int, optional
+            Number of matches used to calculate recent form. Defaults to 5.
+
+        Returns
         -------
         float
-            Win % of the team in their last N matches 
+            Win % of the team in their last N matches
         """
-        pass
+        df = self.df.sort_values(by="date", ascending=False)
+
+        df = df[(df["team1"] == self.name) | (df["team2"] == self.name)]
+        num_matches = min(num_matches, len(df))
+
+        if num_matches == 0:
+            return 0.0
+
+        recent_matches = df.head(num_matches)
+        win_rate = (((recent_matches["winner"] == self.name).sum()) / num_matches) * 100
+
+        return win_rate
 
     def calc_win_rate_defending(self, num_matches=5) -> float:
         """
-        Calculate the win rate of the team in the last N matches they bat first in. 
+        Calculate the win rate of the team in the last N matches they bat first in.
 
         Parameters
         ----------
-        num_matches: int, optional 
-            Number of matches used to calculate the win %. Defaults to 5.              
-        
-        Returns 
+        num_matches: int, optional
+            Number of matches used to calculate the win %. Defaults to 5.
+
+        Returns
         -------
         float
-            Win % of the team the last N times they have batted first 
+            Win % of the team the last N times they have batted first
         """
         pass
 
     def calc_win_rate_chasing(self, num_matches=5) -> float:
         """
-        Calculate the win rate of the team in the last N matches they bowl first in.  
-        
+        Calculate the win rate of the team in the last N matches they bowl first in.
+
         Parameters
         ----------
-        num_matches: int, optional 
-            Number of matches used to calculate the win %. Defaults to 5. 
+        num_matches: int, optional
+            Number of matches used to calculate the win %. Defaults to 5.
 
-        Returns 
+        Returns
         -------
         float
-            Win % of the team the last N times they have bowled first 
+            Win % of the team the last N times they have bowled first
         """
         pass
-    
-    def calc_head_to_head_win_rate(self, opposition:str, num_matches=4) -> float:
+
+    def calc_head_to_head_win_rate(self, opposition: str, num_matches=5) -> float:
         """
         Calculate the win rate of the team against a given opposition in the last N encounters
 
         Parameters
         ---------
-        num_matches: int, optional 
-            Number of matches used to calculate the win %. Defaults to 4.         
-        
-        Returns 
+        opposition: str
+            The opposition used for the head to head win rate
+        num_matches: int, optional
+            Number of matches used to calculate the win %. Defaults to 5.
+
+        Returns
         -------
         float
             Win % of the team against the opposition in the last N matches
         """
         pass
 
+    def calc_venue_win_rate(self, venue: str, num_matches=5) -> float:
+        """
+        Calculate the win rate of the team at a given venue in the last N matches they have played there
+
+        Parameters
+        ----------
+        venue:str
+            The venue to calculate the win rate for
+        num_matches: int, optional
+            Number of matches used to calculate the win %. Defaults to 5.
+
+        Returns
+        -------
+        float
+            Win % of the team at a given venue
+        """
+        pass
+
+    def get_name(self):
+        return self.name
+
+    def set_df(self, new_df: pd.DataFrame):
+        self.df = new_df
